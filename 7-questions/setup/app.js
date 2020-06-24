8a072154661533268e99e5aead0f3e5b0bec915b
yinsweet <yinsweet@gmail.com>
2020-06-25 02:53:14 +0800

    Complete 7-questions

--- 7-questions/setup/app.js
+++ 7-questions/setup/app.js
@@ -1,2 +1,28 @@
-//using selectors inside the element
+// using selectors inside the element
+
+const questions = document.querySelectorAll('.question');
+
+questions.forEach(function (question) {
+  // console.log(question);
+  const btn = question.querySelector('.question-btn');
+  // console.log(btn);
+  btn.addEventListener('click', function () {
+    questions.forEach(function (item) {
+      if (item !== question) {
+        item.classList.remove('show-text');
+      }
+    });
+    question.classList.toggle('show-text');
+  });
+});
+
 // traversing the dom
+// const btns = document.querySelectorAll('.question-btn');
+//
+// btns.forEach(function (btn) {
+//   btn.addEventListener('click', function(e) {
+//     // console.log(e.currentTarget.parentElement.parentElement);
+//     const question = e.currentTarget.parentElement.parentElement;
+//     question.classList.toggle('show-text');
+//   });
+// });
